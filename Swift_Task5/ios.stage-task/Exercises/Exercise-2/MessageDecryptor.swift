8e89c24d2188bb2e179cd9aaa736231b0016a795
Artyom Gurbovich <artyom.gurbovich@gmail.com>
2021-06-14 18:08:29 +0300

    feat: implement exercise 2

--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -3,6 +3,30 @@ import UIKit
 class MessageDecryptor: NSObject {
     
     func decryptMessage(_ message: String) -> String {
-        return ""
+        guard message.count >= 1 && message.count <= 60 else { return String() }
+        var decryptedMessage = message
+        while let range = decryptedMessage.range(of: #"[0-9]+\[[a-z]+\]"#, options: .regularExpression){
+            var index = range.lowerBound
+            var countStr = ""
+            var word = ""
+            while index != range.upperBound {
+                if decryptedMessage[index].isNumber {
+                    countStr.append(decryptedMessage[index])
+                } else if decryptedMessage[index].isLetter {
+                    word.append(decryptedMessage[index])
+                }
+                index = decryptedMessage.index(after: index)
+            }
+            let count = Int(countStr) ?? .zero
+            if count < 1 || count > 300 {
+                return ""
+            }
+            decryptedMessage.removeSubrange(range)
+            decryptedMessage.insert(contentsOf: String.init(repeating: word, count: count), at: range.lowerBound)
+        }
+        if let range = decryptedMessage.range(of: #"[a-z]+"#, options: .regularExpression) {
+            return String(decryptedMessage[range.lowerBound ... decryptedMessage.index(before: (range.upperBound))])
+        }
+        return String()
     }
 }
